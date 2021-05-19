82f985536a3b892a58460ed4c53bef99dc1102d6
荔枝 <sench@lizhideMacBook-Pro.local>
2021-05-20 03:49:57 +0800

    init vue-app

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import "./plugins/axios";
+import "./plugins/element.js";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
